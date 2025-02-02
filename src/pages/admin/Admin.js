76eda160bf549ff2b339aeae7c5de29c5484633e
SnyDrive25 <sunilgouvi@gmail.com>
2025-02-02 01:01:58 +0100

    update admin dashboard

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -7,29 +7,36 @@ function Admin() {
     document.title = "Admin - La Plume De Vinci";
 
     setTimeout(() => {
-        if(sessionStorage.getItem('bsfur') === sha256("D4ctyL3o!").toString()) {
+        if (sessionStorage.getItem('bsfur') === sha256("D4ctyL3o!").toString()) {
             window.location.href = "../connected";
             document.getElementById("message2").textContent = "Vous êtes connecté";
         }
     }, 10);
 
     var Airtable = require('airtable');
-    var base = new Airtable({apiKey: process.env.REACT_APP_API}).base('appWPDVfFKbFOF1eA');
+    var base = new Airtable({ apiKey: process.env.REACT_APP_API }).base('appWPDVfFKbFOF1eA');
 
     async function verifyAdmin() {
-        var pseudo2 = document.getElementById("pseudo2").value;
-        var mdp2 = document.getElementById("mdp2").value;
+        try {
+            var pseudo2 = document.getElementById("pseudo2").value;
+            var mdp2 = document.getElementById("mdp2").value;
 
-        const records = await base('admins').select({
-            filterByFormula: `SEARCH("${pseudo2}",{pseudo} & "")`,
-        }).all();
+            const records = await base('admins').select({
+                filterByFormula: `SEARCH("${pseudo2}",{pseudo} & "")`,
+            }).all();
 
-        if(sha256(mdp2).toString() === records[0].fields.mdp) {
-            sessionStorage.setItem("bsfur", sha256("D4ctyL3o!").toString());
-            sessionStorage.setItem("eltx", pseudo2);
-            window.location.href = "../connected";
+            if (sha256(mdp2).toString() === records[0].fields.mdp) {
+                sessionStorage.setItem("bsfur", sha256("D4ctyL3o!").toString());
+                sessionStorage.setItem("eltx", pseudo2);
+                window.location.href = "../connected";
+            }
+            else {
+                document.getElementById("message2").textContent = "Erreur lors de la connexion. Veuillez réessayer";
+                document.getElementById("pseudo2").value = "";
+                document.getElementById("mdp2").value = "";
+            }
         }
-        else {
+        catch {
             document.getElementById("message2").textContent = "Erreur lors de la connexion. Veuillez réessayer";
             document.getElementById("pseudo2").value = "";
             document.getElementById("mdp2").value = "";
